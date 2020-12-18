5a2ca1a6f02a5797dd7782d30d2c6afd4c6074b4
RobertgSchultz8 <71945176+RobertgSchultz8@users.noreply.github.com>
2020-12-18 10:00:02 -0500

    Add files via upload

---
+++ networkmonitor.sh
@@ -0,0 +1,9 @@
+#should be set to run at midnight every night
+#adding the date the script was run to the output file name allows keeping track of server access over multiple days
+NOW=$(date +"%b-%d-%y")
+tcpdump -q -w accesslog-$NOW.pcap &
+pid=$!
+#originally, I wanted to use cron to have a script job that runs continuously for six hours, but I could not get that to work as well
+#there is probably a way to do that, but I am not sure what it is, so I'll settle for killing the process after six hours
+sleep 6h
+kill $pid
